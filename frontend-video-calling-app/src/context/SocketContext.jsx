d12c6c86754370dddf5cdf8e20f0706539510f97
Alphasf9 <134485025+Alphasf9@users.noreply.github.com>
2025-08-12 14:18:38 +0530

    Add dotenv configuration and update dependencies for environment management

--- frontend-video-calling-app/src/context/SocketContext.jsx
+++ frontend-video-calling-app/src/context/SocketContext.jsx
@@ -1,31 +1,25 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useMemo } from "react";
-import { useContext } from "react";
+import { createContext, useMemo, useContext } from "react";
 import { io } from "socket.io-client";
-
-
 
 const SocketContext = createContext();
 
-
 export const useSocket = () => useContext(SocketContext);
 
-
 export const SocketProvider = ({ children }) => {
-
     const socket = useMemo(() => {
-        return io('http://localhost:8000');
-    }, [])
-
-
-
+        return io(
+            import.meta.env.VITE_BACKEND_URL || "http://localhost:8000",
+            {
+                transports: ["websocket"], 
+                reconnection: true
+            }
+        );
+    }, []);
 
     return (
         <SocketContext.Provider value={socket}>
             {children}
         </SocketContext.Provider>
-    )
-}
-
-
-
+    );
+};
